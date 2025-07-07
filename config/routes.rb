9cba4d741ef0816f000e81c84fdfc50e4987ad19
Eri Higuchi <209428143+ehcy223@users.noreply.github.com>
2025-07-07 15:27:33 +0000

    Usersコントローラを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
-  get 'homes/top'
-  devise_for :users
-  root to: 'homes#top'
+  #エンドユーザー用Deviseルーティング
+  devise_for :users,skip: [:passwords], controllers: {
+    sessions: 'public/sessions',
+    registrations: 'public/registrations',
+  }  
+  
+  #エンドユーザー用ルーティング
+  scope module: :public do
+    root to: 'homes#top'
+
+    get 'mypage' => 'users#mypage', as: 'mypage'
+  end
+  # 管理者用Deviseルーティング
+  devise_for :admin,skip: [:registrations, :passwords] ,controllers:{
+    sessions: 'admin/sessions'
+  }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
